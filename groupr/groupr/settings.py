51b16a1233693bb01016578951c18abf0040e961
Niki Wells <titanswimr@gmail.com>
2016-03-16 19:41:31 -0500

    started gitignore, hooked up db to django

--- groupr/groupr/settings.py
+++ groupr/groupr/settings.py
@@ -76,8 +76,10 @@ WSGI_APPLICATION = 'groupr.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'grouprsp_cs411',
+        'USER': 'groupr',
+        'PASSWORD': 'grouprsp16'
     }
 }
 
