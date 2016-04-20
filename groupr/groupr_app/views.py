9c29f7698f45098bcf758d2078ad01c319781d19
Niki Wells <titanswimr@gmail.com>
2016-04-19 22:36:24 -0500

    comma separation on update student

--- groupr/groupr_app/views.py
+++ groupr/groupr_app/views.py
@@ -137,28 +137,28 @@ def update_student(request, netId):
 
 				# insert known skills
 				if 'skillsKnown' in form.changed_data:
-					known_skills = form.cleaned_data['skillsKnown'].lower().split()
+					known_skills = form.cleaned_data['skillsKnown'].lower().replace(', ', ',').split(',')
 					for skill in known_skills:
 						insert('Skills', {'name': skill})
 						insert('SkillsKnown', {'netId' : student_values['netId'], 'skillName' : skill})
 
 				#insert desired skills
 				if 'skillsDesired' in form.changed_data:
-					desired_skills = form.cleaned_data['skillsDesired'].lower().split()
+					desired_skills = form.cleaned_data['skillsDesired'].lower().replace(', ', ',').split(',')
 					for skill in desired_skills:
 						insert('Skills', {'name': skill})
 						insert('SkillsDesired', {'netId' : student_values['netId'], 'skillName' : skill})
 
 				#insert known languages
 				if 'langsKnown' in form.changed_data:
-					known_langs = form.cleaned_data['langsKnown'].lower().split()
+					known_langs = form.cleaned_data['langsKnown'].lower().replace(', ', ',').split(',')
 					for lang in known_langs:
 						insert('Languages', {'name' : lang})
 						insert('LanguagesKnown', {'netId' : student_values['netId'], 'languageName' : lang})
 
 				#insert desired languages
 				if 'langsDesired' in form.changed_data:
-					desired_langs = form.cleaned_data['langsDesired'].lower().split()
+					desired_langs = form.cleaned_data['langsDesired'].lower().replace(', ', ',').split(',')
 					for lang in desired_langs:
 						insert('Languages', {'name' : lang})
 						insert('LanguagesKnown', {'netId' : student_values['netId'], 'languageName' : lang})
