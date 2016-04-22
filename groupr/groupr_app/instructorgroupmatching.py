790f5979b55181d2dcc4c29b6926e26a1fc26063
Niki Wells <titanswimr@gmail.com>
2016-04-21 20:23:09 -0500

    gonna help with ken's stuff real fast

--- groupr/groupr_app/instructorgroupmatching.py
+++ groupr/groupr_app/instructorgroupmatching.py
@@ -8,6 +8,9 @@ from .queries import select
 lang_des  = 'LanguagesDesired'
 lang_know = 'LanguagesKnown'
 
+min_students = 3
+max_students = 4
+
 def get_students():
 	students = select(['Student'], ['netId'], '')
 	print(students)
@@ -16,13 +19,17 @@ def get_students():
 			select([lang_des], ['languageName'], "netId=\'{}\'".format(student['netId']))])
 		student[lang_know] = set([language['languageName'] for language in 
 			select([lang_know], ['languageName'], "netId=\'{}\'".format(student['netId']))])
+	print(len(students))
 	return students
 
-def number_of_groups(num_students, min, max):
+def number_of_groups(num_students, min_students, max_students):
 	num_students = num_students * 1.0
-	return math.ceil(num_students / min) - (1-math.round(num_students / max)
+	return math.ceil(num_students / min) - (1-math.round(num_students / max))
 
 def make_matches():
 	students = get_students()
+	num_groups = number_of_groups(len(students), min_students, max_students)
+
+	
 
 print(get_students())
